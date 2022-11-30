7a0f70f460c211cafc7d7c8339d7c4a3c87284e7
sayuki2117 <wisdom.osayuki@uniben.edu>
2022-11-30 11:13:38 +0000

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2 != 0:
-        print("{:c}".format(i - 32), end='')
-    else:
-        print("{:c}".format(i), end='')
+for c in range(ord('z'), ord('a') - 1, -2):
+    print("{:c}{:s}".format(c, chr(c - 33)), end="")
