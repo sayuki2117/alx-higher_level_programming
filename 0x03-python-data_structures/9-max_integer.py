79146e4685363f3483840fd6f80ae948df40ed0b
sayuki2117 <wisdom.osayuki@uniben.edu>
2022-12-05 02:10:59 +0000

    9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
-    if len(my_list) == 0:
-        return None
-    maxint = my_list[0]
-    for num in my_list:
-        if num > maxint:
-            maxint = num
-    return maxint
+def divisible_by_2(my_list=[]):
+    list_result = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            list_result.append(True)
+        else:
+            list_result.append(False)
+    return list_result
