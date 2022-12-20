0fe59b8ddbd86799c1c65f3940b66004d6acb4d6
sayuki2117 <wisdom.osayuki@uniben.edu>
2022-12-20 15:20:19 +0000

    1-square.py file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 1-square.py
+# @sayuki2117
+
+""" Class Square defination of a square """
+
+
+class Square:
+    """ Class Square Representation """
+    def __init__(self, size):
+        self.__size = size
