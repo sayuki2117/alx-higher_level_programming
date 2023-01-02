b725e7395c34c3d2ba604b3b123befee6fd9427c
sayuki2117 <wisdom.osayuki@uniben.edu>
2023-01-02 11:20:34 +0000

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# @sayuki2117
+
+"""
+This file contains empty class Rectangle that defines a rectangle
+"""
+
+
+class Rectangle():
+    """
+    This is the class rectangle
+    """
+    pass
