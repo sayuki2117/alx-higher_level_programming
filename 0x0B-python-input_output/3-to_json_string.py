03807dc05f471a12df317fd4a89c9838e7eb8961
sayuki2117 <wisdom.osayuki@uniben.edu>
2023-01-11 04:44:49 +0000

    3-to_json_string.py file

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# @sayuki2117
+"""
+A function that returns 
+JSON representation of obj (string)
+"""
+
+
+def to_json_string(my_obj):
+    """Returns JSON representation of obj (string)
+    Args:
+        my_obj: python object
+    Return:
+        json string representation
+    """
+    import json
+
+    return json.dumps(my_obj)
